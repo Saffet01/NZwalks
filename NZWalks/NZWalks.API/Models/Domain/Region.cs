fb27115abfd362b0fd772679d7067952c6673832
SRK <saffetr.kaban@gmail.com>
2022-10-05 00:20:16 +0300

    Denemeye devam ediyorum :/

---
+++ NZWalks/NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,19 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public long Population { get; set; }
+
+        // Navigation Property
+        public IEnumerable<Walk> Walks { get; set; }
+
+        // 14.satırda Entity framework'e bir regionun birden fazla walk'ı olabilir diyoruz
+        // yani Region objesi ile Walk objesini birbiri ile ilişkilendirdik bu şekilde.
+    }
+}
